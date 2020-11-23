ec25d264092443184b4902312c3fc1520a1ca77b
Chen-Zidi <czdpaige@gmail.com>
2020-11-23 14:39:24 +0100

    fix the bug of answering question after creating a question

--- tasks/Task3/src/main/java/servlet/AnswerQuestionServlet.java
+++ tasks/Task3/src/main/java/servlet/AnswerQuestionServlet.java
@@ -22,14 +22,20 @@ public class AnswerQuestionServlet extends HttpServlet {
         QuestionDAO qd = new QuestionDAO();
         List<Question> questionList = qd.getAllQuestions();
 
+        System.out.println("question list length"+questionList.size());
+
         List<String> answerList = new ArrayList<String>();
 
-        for(int i = 1; i < questionList.size()+1;i++) {
+        for(int i = 0; i < questionList.size();i++) {
 
             //get answers
             String answer = "";
-            String identifier = "question" + Integer.toString(i);
+            String identifier = "question" + Integer.toString(questionList.get(i).getId());
             String[] result = request.getParameterValues(identifier);
+
+            System.out.println("identifier" + identifier);
+
+            System.out.println(result);
 
             //reformat the answers
             for(String s : result){
